452ff9ce3e9f8331f49b95cfb0ad75aeaee03a92
wrotnias <wrotniak.kuba@gmail.com>
2024-01-20 11:05:14 +0100

    Minor improvements

--- timesheet.py
+++ timesheet.py
@@ -19,9 +19,9 @@ bcrypt = Bcrypt(app)
 
 # Formularz logowania
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    submit = SubmitField('Login')
+    username = StringField('Login', validators=[DataRequired()])
+    password = PasswordField('Hasło', validators=[DataRequired()])
+    submit = SubmitField('Zaloguj')
 
 ##################################### MODELE ########################################
 # Model użytkownika
